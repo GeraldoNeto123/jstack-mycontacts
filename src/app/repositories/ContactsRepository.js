92638ce11ed483ff5756015b43fc3cb9c7f995d3
Geraldo Neto <geraldodoliveira123@gmail.com>
2023-02-26 10:41:07 -0300

    Iniciando api

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,85 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Geraldo Neto',
+    email: 'geraldo@gmail.com',
+    phone: '1111111',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Teste',
+    email: 'teste@gmail.com',
+    phone: '1111111',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updateContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map(
+        (contact) => (contact.id === id ? updateContact : contact),
+      );
+
+      resolve(updateContact);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
